35ba7a9b58cc6d05563c0e51307518a1018e6196
muyong <yong@max2.com>
2018-04-15 13:39:02 -0400

    Initial commit

---
+++ src/MyClientEndpoint.java
@@ -0,0 +1,16 @@
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
+import java.text.SimpleDateFormat;
+
+@ClientEndpoint(encoders = MessageEncoder.class, decoders = MessageDecoder.class)
+public class MyClientEndpoint {
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+
+    @OnMessage
+    public void onMessage(Message message) {
+        System.out.println(String.format("[%s:%s] %s",
+                simpleDateFormat.format(message.getReceived()), message.getSender(), message.getContent()));
+    }
+
+}
